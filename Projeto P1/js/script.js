75fba1bf3f52d3a5196067bef72b362b3ebd1ecd
Arthur Hayakawa <ayhayakawa@gmail.com>
2022-10-03 17:54:35 -0300

    pre-0.1

--- Projeto P1/js/script.js
+++ Projeto P1/js/script.js
@@ -1,1 +1,22 @@
-//
+function botao_cadastrar() {
+    var form1 = document.getElementById("caixa-1");
+    var form2 = document.getElementById("caixa-2");
+    
+    form1.style.display = "none";
+    form1.style.visibility = "hidden";
+
+    form2.style.display = "flex"
+    form2.style.display = "visible"
+}
+
+function botao_cadastro_reexibir() {
+    var form1 = document.getElementById("caixa-1");
+    var form2 = document.getElementById("caixa-2");
+
+
+    form1.style.display = "flex";
+    form1.style.visibility = "visible";
+
+    form2.style.display = "none"
+    form2.style.display = "hidden"
+}
